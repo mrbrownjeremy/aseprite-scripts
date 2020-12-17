e7e384bf11ae5c76c3ba62a5c608fa7e8c5da0cc
quietly-turning <quietly.turning@gmail.com>
2020-12-17 12:41:54 -0500

    add script for Duplicate+Link Cels

---
+++ scripts/Duplicate and Link Cels.lua
@@ -0,0 +1,45 @@
+---------------------------------------------------------------
+-- Duplicate and Link Cels
+--
+-- author: quietly-turning
+-- github: https://github.com/quietly-turning/
+---------------------------------------------------------------
+
+if not app then return end
+if not (app.isUIAvailable and app.activeCel) then return end
+if not app.range.type == RangeType.CELS then return end
+
+---------------------------------------------------------------
+
+local dlg = Dialog("Duplicate & Link")
+
+---------------------------------------------------------------
+
+local DupAndLink = function()
+	local frame_count = dlg.data.frame_count - 1
+
+	for i=1, frame_count do
+		app.command.NewFrame({content="cellinked"})
+	end
+
+	app.refresh()
+end
+
+---------------------------------------------------------------
+
+dlg:number({
+	id = "frame_count",
+	label = "Num Frames:",
+	text = "",
+	focus = true,
+})
+
+
+dlg:button({
+	id = "FadeOut_button",
+	text = "Duplicate",
+	onclick = function() app.transaction( DupAndLink ) end
+})
+
+-- allow the user to continue to interact with Aseprite while this dialog is up
+dlg:show({wait=false})
