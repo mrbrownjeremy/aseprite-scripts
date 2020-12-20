f1d03186bf00ead19e1d3bfa16c27098e6bff750
quietly-turning <quietly.turning@gmail.com>
2020-12-20 07:10:27 -0500

    update Duplicate+LinkCels w/ link or copy checkbox
    This updates the Duplicate and Link Cels dialog to include
       label that shows the frameNumber range
    
       checkbox for "Link Cels", checked by default
          cels will be copied if unchecked
    
       checkbox for "Return to Start", checked by default
          activeCel will be first cel if checked
          activeCel will be last cel if unchecked

--- scripts/Duplicate and Link Cels.lua
+++ scripts/Duplicate and Link Cels.lua
@@ -19,8 +19,10 @@ local DupAndLink = function()
 	local frame_count = dlg.data.frame_count - 1
 
 	for i=1, frame_count do
-		app.command.NewFrame({content="cellinked"})
+		app.command.NewFrame({content=(dlg.data.LinkCels_checkbox and "cellinked" or "celcopies")})
 	end
+
+	app.activeCel = app.activeLayer:cel( app.activeCel.frameNumber - (dlg.data.ReturnToStart_checkbox and frame_count or 0) )
 
 	app.refresh()
 end
@@ -29,9 +31,44 @@ end
 
 dlg:number({
 	id = "frame_count",
-	label = "Num Frames:",
+	label = "   Num Frames:",
 	text = "",
 	focus = true,
+	-- update the range label when frame_count changes
+	onchange=function()
+		local range_text
+
+		if dlg.data.frame_count > 0 then
+			range_text = ("%d – %d"):format(app.activeCel.frameNumber, app.activeCel.frameNumber+(dlg.data.frame_count-1))
+		else
+			range_text = ""
+		end
+
+		dlg:modify({
+			id="range_label",
+			text=range_text
+		})
+	end
+})
+
+dlg:label({
+	id = "range_label",
+	label = "          Range:",
+	text = "",
+})
+
+dlg:separator()
+
+dlg:check({
+	id = "LinkCels_checkbox",
+	label="       Link Cels:",
+	selected=true,
+})
+
+dlg:check({
+	id = "ReturnToStart_checkbox",
+	label="Return to Start:",
+	selected=true,
 })
 
 
@@ -40,6 +77,8 @@ dlg:button({
 	text = "Duplicate",
 	onclick = function() app.transaction( DupAndLink ) end
 })
+
+
 
 -- allow the user to continue to interact with Aseprite while this dialog is up
 dlg:show({wait=false})
